9b07972747efc02b66bca72c555887dba4ccad24
prashant tak <prashantt4333@gmail.com>
2024-10-16 19:49:38 +0530

    create pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
-
+import { BrowserRouter,Routes,Route } from "react-router-dom"
+import Home from "./pages/Home"
+import About from "./pages/About"
+import Projects from "./pages/Projects"
+import Dashboard from "./pages/Dashboard"
+import SignIn from "./pages/SignIn"
+import SignUp from "./pages/SignUp"
 
 function App() {
 
   return (
-    <>
-              
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} /> 
+        <Route path="/about" element={<About />} /> 
+        <Route path="/projects" element={<Projects />} /> 
+        <Route path="/dashboard" element={<Dashboard />} /> 
+
+        <Route path="/sign-in" element={<SignIn />} /> 
+        <Route path="/sign-up" element={<SignUp />} /> 
+      </Routes>
+    </BrowserRouter>
   )
 }
 
